22fee72599c56605b2d7f25d9a8fd1842d395380
folen1 <133991378+folen1@users.noreply.github.com>
2023-05-20 16:21:20 +0300

    Update pin-pong.py
    Проект закончен

--- pin-pong.py
+++ pin-pong.py
@@ -35,49 +35,71 @@ class GameSprite(sprite.Sprite):
 class Play_game(GameSprite):
     def update(self):
         keys_pressed=key.get_pressed()
-        if keys_pressed[K_a]and self.rect.x <700:
-            self.rect.x-=10
-        if keys_pressed[K_d]and self.rect.x <700:
-            self.rect.x+=10
-        for e in event.get():
-            if e.type==QUIT:
-                game = False
+        if keys_pressed[K_UP]and self.rect.y > 5:
+            self.rect.y-=10
+        if keys_pressed[K_DOWN]and self.rect.y < 500:
+            self.rect.y+=10
+    def update_r(self):
+        keys_pressed=key.get_pressed()
+        if keys_pressed[K_w]and self.rect.y > 5:
+            self.rect.y-=10
+        if keys_pressed[K_s]and self.rect.y <500:
+            self.rect.y+=10
+
     
 
 hero= Play_game('platform.png',670,100,5,20,150)
 hero1 = Play_game('platform.png',10,100,5,20,150)
+ball = GameSprite('ball_1615463127.png',300,100,5,50,50)
 lost1 =0
 
 monsters = sprite.Group()
-win1=font1.render('=)',True,(255,255,255))
+
 text_lose=font1.render('=(',True,(255,255,255))
 bullets = sprite.Group()
-
+win_height = 500
 finish=False
 game = True
-health=5
+speed_x = 3
+speed_y = 3
 while game:
+
     for e in event.get():
         if e.type==QUIT:
             game = False
-        elif e.type ==KEYDOWN:
-            if e.key==K_w:
-                hero.fire()
+    
 
     if finish==False:
-        keys_pressed=key.get_pressed()
+        ball.rect.x += speed_x
+        ball.rect.y +=speed_y
         window.blit(background,(0,0))
+    if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+        speed_y *= -1        
+    if sprite.collide_rect(hero, ball) or sprite.collide_rect(hero1, ball):
+        speed_x *= -1 
 
-
-
+        
+    if ball.rect.x > 650:
+        win1=font1.render('Победил первый игрок',True,(255,255,255))
+        window.blit(win1,(200,200))
+        finish = True
+    if ball.rect.x < 10:
+        win2=font1.render('Победил второй игрок',True,(255,255,255))
+        window.blit(win2,(200,200))
+        finish = True
 
     keys_pressed=key.get_pressed()
 
     hero.reset()
     hero1.reset()
+    ball.reset()
+
+    
+
     hero.update()
+    hero1.update_r()
+
 
 
     clock.tick(60)
     pygame.display.update()
-
